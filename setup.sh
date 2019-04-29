985663bcb2ef610acab51aa833bbd83e587d4ab8
beerfekt <christoph.beer@gmx.de>
2019-04-29 12:14:13 +0200

    upated setup.sh

--- setup.sh
+++ setup.sh
@@ -26,4 +26,25 @@ echo "Fertig! \n"
 sudo  sed -i -e "/$URLFRONTEND/d" /etc/hosts; sudo sed -i "2i127.0.3.1 $URLFRONTEND"  /etc/hosts
 sudo  sed -i -e "/$URLBACKEND/d" /etc/hosts; sudo sed -i "2i127.0.3.2 $URLBACKEND" /etc/hosts
 
+
+
+#installing dependencies
+
+#TODO: alle dependencies reinziehen aus der anderen setup.sh (vagrant)
+#TODO: node-modules löschen wenn fertig
+
+#docker exec docker-rest-frontend-server apk add --update nodejs nodejs-npm
+#log in
+docker exec -it docker-rest-frontend-server sh
+#npm
+apk add --update nodejs nodejs-npm
+#ui-router
+npm install --save @uirouter/angularjs
+#toaster
+npm install angular-toastr
+
+
+
+
+
 #TODO: npm installieren und abhängigkeiten reinziehen, db dump einspielen, domain namen für service verteilen 
